67f7384ea1e137a326dd1ea8ac9035be41c1a433
lleme2 <lucaslopesleme4@gmail.com>
2023-05-23 13:11:39 -0300

    Adição de arquivos login e cadastro

---
+++ login_sigin/empresas.js
@@ -0,0 +1,21 @@
+const { ipcRenderer } = require('electron');
+
+const button_signin = document.getElementById("data");
+const button_login = document.getElementById("login");
+const email = document.getElementById('user');
+const password = document.getElementById('password');
+
+button_signin.addEventListener('submit', (e) => {
+    e.preventDefault();
+    try{
+        console.log("botao");
+        const obj = {
+            email: email.value,
+            password: password.value
+        }
+        ipcRenderer.invoke('some-name', obj);
+    }
+    catch{
+        console.log(e);
+    }
+})
